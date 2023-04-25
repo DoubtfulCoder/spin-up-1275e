0ee2de4740d71261f7e31979c95cb286e417f9c9
piman51277 <piman.dev@gmail.com>
2023-04-25 08:23:09 -0400

    fix base64 encode/decode

--- src/lib/util/fromBase64.cpp
+++ src/lib/util/fromBase64.cpp
@@ -6,18 +6,13 @@ std::bitset<44> fromBase64(std::string base64)
 
   std::string base64Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-  // convert to string by converting 6 bits at a time (last bits are 0-padded)
-  for (int i = 0; i < 44; i += 6)
+  // convert to string by converting 1 char at a time
+  for(int i = 0; i < base64.length(); i++)
   {
-    int val = 0;
-    for (int j = 0; j < 6; j++)
+    int val = base64Chars.find(base64[i]);
+    for(int j = 0; j < 6; j++)
     {
-      val <<= 1;
-      val |= base64Chars.find(base64[i + j]);
-    }
-    for (int j = 0; j < 6; j++)
-    {
-      out[i + j] = val & 1;
+      out[i * 6 + j] = val & 1;
       val >>= 1;
     }
   }
--- src/lib/util/toBase64.cpp
+++ src/lib/util/toBase64.cpp
@@ -2,20 +2,27 @@
 
 std::string toBase64(std::bitset<44> bits)
 {
+
+  std::bitset<48> extended;
+
+  for(int i = 0; i < 44; i++)
+  {
+    extended[i] = bits[i];
+  }
+
   std::string out;
 
   std::string base64Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
   // convert to string by converting 6 bits at a time (last bits are 0-padded)
-  for (int i = 0; i < 44; i += 6)
+  for (int i = 0; i < 8; i++)
   {
-    int val = 0;
+    std::bitset<8> byte;
     for (int j = 0; j < 6; j++)
     {
-      val <<= 1;
-      val |= bits[i + j];
+      byte[j] = bits[i * 6 + j];
     }
-    out += base64Chars[val];
+    out += base64Chars[byte.to_ulong()];
   }
 
   return out;
